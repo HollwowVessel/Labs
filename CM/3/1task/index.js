2110a49028d96f8bc08b9e7f6a9a88085b0a2d70
Hollow Vessel <mhollow782@gmail.com>
2022-10-04 18:10:42 +0300

    Completed 3d lab

--- CM/3/1task/index.js
+++ CM/3/1task/index.js
@@ -9,48 +9,36 @@ let A = [
 	[0.71, -0.65, -0.18, 0.17],
 	[1.17, -2.35, 0.75, 1.28],
 ];
+let E = 0.0001;
+let prec = 10 ** (String(E).length - 2);
 
 function gaussianElimination() {
-	for (let i = 0; i < 3; i++) {
-		for (let j = i + 1; j < 3; j++) {
-			let c = A[j][i] / A[i][i];
-			for (let k = i + 1; k < 4; k++) {
-				A[j][k] = A[j][k] - A[i][k] * c;
+	for (let i = 0; i < A.length; i++) {
+		for (let j = 0; j < A.length; j++) {
+			if (i !== j) {
+				let temp = A[j][i] / A[i][i];
+				for (let k = 0; k < A.length + 1; k++) {
+					A[j][k] -= temp * A[i][k];
+				}
 			}
-			A[j][i] = 0;
 		}
-	}
-}
-
-function createDiagonalMatrix() {
-	for (let i = 2; i >= 0; i--) {
-		let x = A[i][3] / A[i][i];
-		for (let j = i - 1; j >= 0; j--) {
-			A[j][3] -= x * A[j][i];
-			A[j][i] = 0;
-		}
-		A[i][3] = x;
-		A[i][i] = 1;
 	}
 }
 
 function getResult() {
 	let x = [];
-	for (let i = 0; i < 3; i++) {
-		x.push(Math.round(A[i][3] * 1000) / 1000);
+	for (let i = 0; i < A.length; i++) {
+		x.push(Math.round((A[i][3] / A[i][i]) * prec) / prec);
 	}
 	return x;
 }
 
 function solve() {
 	gaussianElimination();
-	createDiagonalMatrix();
-	let x = getResult();
+	x = getResult();
 	return x;
 }
+res = solve();
+console.log(...res);
 
-let x = solve(A);
-
-console.log(...x);
-
-// Result is: 1.331, 0.693, 1.802
+// Result is: 1.3309, 0.6931, 1.8022
